2946ae460813b865043b0cd743bb73899e1dd0a8
Gordo <60603572+gordo-labs@users.noreply.github.com>
2024-02-21 12:55:40 +0100

    add classnames

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,6 +1,6 @@
 import ConnectButton from "@/components/ConnectButton";
 import Grid from "@/components/Grid";
-
+import Image from "next/image";
 export default function Home() {
   // const result = useReadContract({
   //   abi: UMDPAbi,
@@ -13,43 +13,55 @@ export default function Home() {
     <main className="flex justify-center">
       <Grid>
         <div className="mt-3 col-start-1 col-end-13 w-full flex justify-between items-center border-2 bg-white/50 border-rose-300 backdrop-blur-sm rounded-full p-2">
-          <p>Mufi</p>
+          <div className="flex items-center gap-2">
+            <Image
+              src="/logo.svg"
+              alt="Logo"
+              width={120} // Replace with actual logo size
+              height={60} // Replace with actual logo size
+              className="md:inline-block hidden"
+            />
+            <p className="text-sm font-light">Alpha v0.1</p>
+          </div>
           <ConnectButton />
         </div>
 
-
         <ol className="col-start-1 col-end-13 p-2 items-center w-full space-y-4 sm:flex sm:space-x-8 sm:space-y-0 rtl:space-x-reverse">
-    <li className="flex items-center text-rose-600 dark:text-rose-500 space-x-2.5 rtl:space-x-reverse">
-        <span className="flex items-center justify-center w-8 h-8 border border-rose-600 rounded-full shrink-0 dark:border-rose-500">
-            1
-        </span>
-        <span>
-            <h3 className="font-medium leading-tight">Select or create the Music NFT</h3>
-            <p className="text-sm">Step details here</p>
-        </span>
-    </li>
-    <li className="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5 rtl:space-x-reverse">
-        <span className="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
-            2
-        </span>
-        <span>
-            <h3 className="font-medium leading-tight">Stablish Royalties distribution</h3>
-            <p className="text-sm">Step details here</p>
-        </span>
-    </li>
-    <li className="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5 rtl:space-x-reverse">
-        <span className="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
-            3
-        </span>
-        <span>
-            <h3 className="font-medium leading-tight">Connect DPSs payments</h3>
-            <p className="text-sm">Step details here</p>
-        </span>
-    </li>
-</ol>
-{/* 
+          <li className="flex items-center text-rose-600 dark:text-rose-500 space-x-2.5 rtl:space-x-reverse">
+            <span className="flex items-center justify-center w-8 h-8 border border-rose-600 rounded-full shrink-0 dark:border-rose-500">
+              1
+            </span>
+            <span>
+              <h3 className="font-medium leading-tight">
+                Select or create the Music NFT
+              </h3>
+              <p className="text-sm">Step details here</p>
+            </span>
+          </li>
+          <li className="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5 rtl:space-x-reverse">
+            <span className="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
+              2
+            </span>
+            <span>
+              <h3 className="font-medium leading-tight">
+                Stablish Royalties distribution
+              </h3>
+              <p className="text-sm">Step details here</p>
+            </span>
+          </li>
+          <li className="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5 rtl:space-x-reverse">
+            <span className="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
+              3
+            </span>
+            <span>
+              <h3 className="font-medium leading-tight">Connect DPSs</h3>
+              <p className="text-sm">Step details here</p>
+            </span>
+          </li>
+        </ol>
+        {/* 
 STEP 1
- */}        
+ */}
         <div className="col-start-1 col-end-13 w-full flex flex-col border-2 bg-white/50 border-rose-300 backdrop-blur-sm rounded-lg p-2">
           <h2 className="mb-6">Select a music NFT to distribute:</h2>
 
@@ -73,10 +85,14 @@ STEP 1
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-rose-500 focus:border-rose-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-rose-500 dark:focus:border-rose-500"
             />
           </div>
-          <button type="submit" className="text-white mt-2 bg-rose-700 hover:bg-rose-800 focus:ring-4 focus:outline-none focus:ring-rose-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-rose-600 dark:hover:bg-rose-700 dark:focus:ring-rose-800">Select NFT</button>
+          <button
+            type="submit"
+            className="text-white mt-2 bg-rose-700 hover:bg-rose-800 focus:ring-4 focus:outline-none focus:ring-rose-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-rose-600 dark:hover:bg-rose-700 dark:focus:ring-rose-800">
+            Select NFT
+          </button>
         </div>
 
-{/* 
+        {/* 
 STEP 2
  */}
         <div className="col-start-1 col-end-13 w-full flex flex-col border-2 bg-white/50 border-rose-300 backdrop-blur-sm rounded-lg p-2">
@@ -102,7 +118,11 @@ STEP 2
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-rose-500 focus:border-rose-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-rose-500 dark:focus:border-rose-500"
             />
           </div>
-          <button type="submit" className="text-white mt-2 bg-rose-700 hover:bg-rose-800 focus:ring-4 focus:outline-none focus:ring-rose-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-rose-600 dark:hover:bg-rose-700 dark:focus:ring-rose-800">Select NFT</button>
+          <button
+            type="submit"
+            className="text-white mt-2 bg-rose-700 hover:bg-rose-800 focus:ring-4 focus:outline-none focus:ring-rose-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-rose-600 dark:hover:bg-rose-700 dark:focus:ring-rose-800">
+            Select NFT
+          </button>
         </div>
       </Grid>
     </main>
