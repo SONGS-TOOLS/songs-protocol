7120b5d91d2c0a7a4f6728c87335312ac2162f31
@mrsalitre <ayalajeancarlos.91@gmail.com>
2024-11-07 14:48:49 +0000

    test: wrapped song factory test passed

--- test/DistributorWalletFactory.ts
+++ test/DistributorWalletFactory.ts
@@ -1,4 +1,3 @@
-import { ethers } from "hardhat";
 import { expect } from 'chai';
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { deployProtocolFixture as deployContractFixture } from './fixtures/protocolFixture';
