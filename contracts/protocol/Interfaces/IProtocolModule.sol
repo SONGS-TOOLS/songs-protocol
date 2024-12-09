9753c0108dce61b8493c0185a7c44a09d4c468fd
Gordo <60603572+gordo-labs@users.noreply.github.com>
2024-12-09 08:26:01 +0100

    Stable for base (#28)
    * stablecoin fees testing
    
    * base

--- contracts/protocol/Interfaces/IProtocolModule.sol
+++ contracts/protocol/Interfaces/IProtocolModule.sol
@@ -6,7 +6,9 @@ import "./ILegalContractMetadata.sol";
 import "./IMetadataRenderer.sol";
 
 interface IProtocolModule {
-    // View functions
+    /**************************************************************************
+     * View functions
+     *************************************************************************/
     function distributorWalletFactory() external view returns (address);
     function whitelistingManager() external view returns (address);
     function paused() external view returns (bool);
@@ -14,61 +16,64 @@ interface IProtocolModule {
     function legalContractMetadata() external view returns (ILegalContractMetadata);
     function owner() external view returns (address);
     function metadataRenderer() external view returns (IMetadataRenderer);
+    function wrappedSongFactoryAddress() external view returns (address);
+    function getOwnerWrappedSongs(address owner) external view returns (address[] memory);
+    function smartAccountToWSToken(address smartAccount) external view returns (address);
+    function maxSaleDuration() external view returns (uint256);
+    function isWSTokenFromProtocol(address wsTokenManagement) external view returns (bool);
+    function getBaseURI() external view returns (string memory);
+    function getStablecoinFeeReceiver() external view returns (address);
+    function getLegalContractMetadata() external view returns (address);
+    function getMetadataModule() external view returns (address);
+    function getRegistryModule() external view returns (address);
+    function isValidToCreateWrappedSong(address creator) external view returns (bool);
+    function isTokenWhitelisted(address token) external view returns (bool);
+    function isAuthorizedContract(address contractAddress) external view returns (bool);
 
-    // State-changing functions
+    /**************************************************************************
+     * Pause
+     *************************************************************************/
     function pause() external;
     function unpause() external;
 
-    // Contract setters
+    /**************************************************************************
+     * Whitelisting
+     *************************************************************************/
+    function whitelistToken(address token) external;
+    function removeTokenFromWhitelist(address token) external;
+
+    /**************************************************************************
+     * Set protocol modules
+     *************************************************************************/
     function setDistributorWalletFactory(address _newFactory) external;
     function setWhitelistingManager(address _whitelistingManager) external;
     function setMetadataModule(address _metadataModule) external;
     function setLegalContractMetadata(address _legalContractMetadata) external;
     function setMetadataRenderer(address _renderer) external;
-
-    // Whitelist management
-    function whitelistToken(address token) external;
-    function removeTokenFromWhitelist(address token) external;
-
-    // Authorization management
+    function setRegistryModule(address _registryModule) external;
+    function setERC20Whitelist(address _erc20whitelist) external;
     function setAuthorizedContract(address contractAddress, bool isAuthorized) external;
 
-    // Factory and token management
-    function wrappedSongFactoryAddress() external view returns (address);
-    function setWrappedSongFactory(address _wrappedSongFactory) external;
-    function setOwnerWrappedSong(address owner, address wrappedSong) external;
+    /**************************************************************************
+     * Set protocol relations
+     *************************************************************************/
     function setSmartAccountToWSToken(address smartAccount, address wsToken) external;
-    function getOwnerWrappedSongs(address owner) external view returns (address[] memory);
-    function smartAccountToWSToken(address smartAccount) external view returns (address);
-
-    // Sale duration management
-    function maxSaleDuration() external view returns (uint256);
     function setMaxSaleDuration(uint256 _duration) external;
-
-    // Protocol token management
-    function isWSTokenFromProtocol(address wsTokenManagement) external view returns (bool);
     function setWSTokenFromProtocol(address wsTokenManagement) external;
-
-    // URI management
-    function getBaseURI() external view returns (string memory);
-    function setBaseURI(string memory _baseURI) external;
-    function getStablecoinFeeReceiver() external view returns (address);
+    function setOwnerWrappedSong(address owner, address wrappedSong) external;
     function setStablecoinFeeReceiver(address newReceiver) external;
 
-    // Metadata management
-    function getLegalContractMetadata() external view returns (address);
-    function getMetadataModule() external view returns (address);
+    /**************************************************************************
+     * Wrapped Song Owner
+     *************************************************************************/
     function renderTokenURI(
         IMetadataModule.Metadata memory metadata,
         uint256 tokenId,
         address wrappedSong
     ) external view returns (string memory);
 
-    function getRegistryModule() external view returns (address);
-    function setRegistryModule(address _registryModule) external;
-
-    // Additional functions
-    function isValidToCreateWrappedSong(address creator) external view returns (bool);
-    function isTokenWhitelisted(address token) external view returns (bool);
-    function isAuthorizedContract(address contractAddress) external view returns (bool);
+    /**************************************************************************
+     * Globals
+     *************************************************************************/
+    function setBaseURI(string memory _baseURI) external;
 }
